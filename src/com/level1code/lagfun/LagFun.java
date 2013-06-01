04cd7db72bca5f00994f7fe21edd4d1676f37d5e
level1kid <dylan96hart@gmail.com>
2013-06-01 13:53:27 -0700

    update to v1.1.0

--- src/com/level1code/lagfun/LagFun.java
+++ src/com/level1code/lagfun/LagFun.java
@@ -6,12 +6,28 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LagFun extends JavaPlugin implements Listener {
+	
+	private String regex; // "\\b[Ll]+\\s*[Aa]+\\s*[Gg]+"
+	
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+		
+		saveDefaultConfig();
+		regex = getConfig().getString("regex");
+		
+		getLogger().info("loading with regex: '" + regex + "'");
 	}
  
+	public void onDisable() {
+	}
+
+	public void onReload() {
+		reloadConfig();
+		regex = getConfig().getString("regex");
+	}
+	
 	@EventHandler
 	public void chat(AsyncPlayerChatEvent event) {
-		event.setMessage(event.getMessage().replaceAll("\\b[Ll]+[Aa]+[Gg]+", "fun"));
+		event.setMessage(event.getMessage().replaceAll(regex, "fun"));
 	}
 }
